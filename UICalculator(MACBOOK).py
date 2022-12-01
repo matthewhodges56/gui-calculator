a91f28dc9f0bb675e9afeb1ec8e1fab9bbbc7021
curlyjet <52683582+curlyjet@users.noreply.github.com>
2022-12-01 11:39:30 -0600

    Update UICalculator(MACBOOK).py

--- UICalculator(MACBOOK).py
+++ UICalculator(MACBOOK).py
@@ -1,113 +1,96 @@
 from tkinter import *
 from tkinter import Tk
 from tkmacosx import Button
+from tkinter import messagebox
 
 window = Tk()
 
 window.geometry('625x425+10+10')
 
-def nine():
-    textbox.insert("end", "9")
+# Button Function
+def insert(function, show):
+    if function == "clear":
+        textbox.delete(0, 'end') # Clear textbox
+    elif function == "delete":
+        textbox.delete(textbox.index("end")-1) # Delete last entered
+    elif function == "equal":
+        expression = textbox.get() # Get current value of textbox
+        try:
+            value = eval(expression) # Evaluate the math problem
+            messagebox.showinfo("Answer", "Answer = " + str(value)) # Popup window with answer
+        except NameError: # Catch any name errors
+            messagebox.showerror("Error", "Only numbers and available functions are allowed!")
+        except ZeroDivisionError: # Catch any divide/multiply by 0 erros
+            messagebox.showinfo("Answer", "Answer = 0")
+        textbox.delete(0, 'end') # Clear textbox after problem
+    else:
+        textbox.insert("end", show) # Enter number/function pressed
 
-def eight():
-    textbox.insert(1, "8")
-
-def seven():
-    textbox.insert(1, "7")
-
-def six():
-    textbox.insert(1, "6")
-
-def five():
-    textbox.insert(1, "5")
-
-def four():
-    textbox.insert(1, "4")
-
-def three():
-    textbox.insert(1, "3")
-
-def two():
-    textbox.insert(1, "2")
-
-def one():
-    textbox.insert(1, "1")
-
-def zero():
-    textbox.insert(1, "0")
-
-def clear():
-    textbox.delete(0, 'end')
-
-nine = Button(window, command = nine, text = "9", fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="blue")
+# Right Side Buttons (Numbers)
+nine = Button(window, command = lambda: insert("nine", "9"), text = "9", fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="blue")
 nine.place(x=550,y=50)
 
-eight = Button(window, command = eight, text= "8",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
+eight = Button(window, command = lambda: insert("eight", "8"), text= "8",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
 eight.place(x=450,y=50)
 
-seven = Button(window, command = seven, text= "7",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
+seven = Button(window, command = lambda: insert("seven", "7"), text= "7",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
 seven.place(x=350,y=50)
 
-six = Button(window, command = six, text= "6",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
+six = Button(window, command = lambda: insert("six", "6"), text= "6",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
 six.place(x=550,y=150)
 
-five = Button(window, command = five, text= "5",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
+five = Button(window, command = lambda: insert("five", "5"), text= "5",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
 five.place(x=450,y=150)
 
-four = Button(window, command = four, text= "4",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
+four = Button(window, command = lambda: insert("four", "4"), text= "4",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
 four.place(x=350,y=150)
 
-three = Button(window, command = three, text= "3",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
+three = Button(window, command = lambda: insert("three", "3"), text= "3",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
 three.place(x=550,y=250)
 
-two = Button(window, command = two, text= "2",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
+two = Button(window, command = lambda: insert("two", "2"), text= "2",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
 two.place(x=450,y=250)
 
-one = Button(window, command = one, text= "1",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
+one = Button(window, command = lambda: insert("one", "1"), text= "1",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
 one.place(x=350,y=250)
 
-delete = Button(window, text= "Del",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
+delete = Button(window, command = lambda: insert("delete", "⌫"), text= "⌫",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
 delete.place(x=550,y=350)
 
-decimal = Button(window, text= ".",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
+decimal = Button(window, command = lambda: insert("decimal", "."), text= ".",fg = "white", width = 45, height = 45, bg="black", border=5, bordercolor="white")
 decimal.place(x=450,y=350)
 
-zero = Button(window, command = zero, text= "0", fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
+zero = Button(window, command = lambda: insert("zero", "0"), text= "0", fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
 zero.place(x=350,y=350)
 
-plus = Button(window, text= "+",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
-plus.place(x=25,y=150)
+# Left Side Buttons (Functions)
+plus = Button(window, command = lambda: insert("plus", "+"), text= "+",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
+plus.place(x=25,y=250)
 
-subtract = Button(window, text= "-",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
-subtract.place(x=100,y=150)
+subtract = Button(window, command = lambda: insert("subtract", "-"), text= "-",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
+subtract.place(x=100,y=250)
 
-multiply = Button(window, text= "x",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
-multiply.place(x=175,y=150)
+multiply = Button(window, command = lambda: insert("multiply", "*"), text= "*",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
+multiply.place(x=175,y=250)
 
-divide = Button(window, text= "÷",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
-divide.place(x=250,y=150)
+divide = Button(window, command = lambda: insert("divide", "/"), text= "/",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
+divide.place(x=250,y=250)
 
-exponent = Button(window, text= "*",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
-exponent.place(x=25,y=250)
-
-percent = Button(window, text= "%",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
-percent.place(x=100,y=250)
-
-pi = Button(window, text= "π",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
-pi.place(x=175,y=250)
-
-para = Button(window, text= "()",fg = "white", width = 45, height = 45, bg="black", bordercolor="white", border=5)
-para.place(x=250,y=250)
-
-clear = Button(window, command = clear, text= "Clear",fg = "white", width = 123, height = 47, bg="black", bordercolor="white", border=5)
+clear = Button(window, command = lambda: insert("clear", "clear"), text= "Clear",fg = "white", width = 123, height = 47, bg="black", bordercolor="white", border=5)
 clear.place(x=25,y=350)
 
-equal = Button(window, text= "=",fg = "white", width = 123, height = 47, bg="black", bordercolor="white", border=5)
+equal = Button(window, command = lambda: insert("equal", "equal"), text= "=",fg = "white", width = 123, height = 47, bg="black", bordercolor="white", border=5)
 equal.place(x=175,y=350)
 
-textbox = Entry(window, width = 20, font = ("Ariel 20"))
-textbox.place(x=21,y=55)
+# Textbox
+textbox = Entry(window, width = 8, font = ("Arial 60"))
+textbox.place(x=22,y=125)
 
+# Label
+label = Label(window, bg = "#53585B", text = "Simple Calculator", font = ("Arial 30"))
+label.place(x = 43, y = 55)
+
+# Display Configurations
 window.configure(bg="#53585B")
 window.title("Calculator")
 window.resizable(False, False)
